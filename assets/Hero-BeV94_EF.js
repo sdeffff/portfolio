bd809627b0cd60dc1f0bf8dbe688141705d11713
Maksym <maxpavliy@gmail.com>
2024-08-31 18:49:06 +0200

    Updates

--- assets/Hero-BeV94_EF.js
+++ assets/Hero-BeV94_EF.js
@@ -1,4 +1,4 @@
-import{r as ir,j as Ot,L as ul}from"./index-DCysxEBL.js";/**
+import{r as ir,j as Ot,L as ul}from"./index-Da5ZyI60.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
